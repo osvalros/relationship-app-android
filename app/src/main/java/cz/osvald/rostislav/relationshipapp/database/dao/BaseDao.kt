af7b20c79f03875b7fbf6b5882efde9dfddec198
Rostislav Osvald <rostislav.osvald@gmail.com>
2021-11-09 23:59:40 +0100

    Extend BaseDao with update function

--- app/src/main/java/cz/osvald/rostislav/relationshipapp/database/dao/BaseDao.kt
+++ app/src/main/java/cz/osvald/rostislav/relationshipapp/database/dao/BaseDao.kt
@@ -2,11 +2,15 @@ package cz.osvald.rostislav.relationshipapp.database.dao
 
 import androidx.room.Delete
 import androidx.room.Insert
+import androidx.room.Update
 
 
 interface BaseDao<T> {
     @Insert
     fun insertAll(vararg users: T)
+
+    @Update
+    fun updateAll(vararg users: T)
 
     @Delete
     fun delete(user: T)
