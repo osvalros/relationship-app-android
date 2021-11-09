989e5ad291233b2557aa0095a4bb2578d92b4e73
Rostislav Osvald <rostislav.osvald@gmail.com>
2021-11-09 23:50:59 +0100

    Add singleton capabilities to database class

--- app/src/main/java/cz/osvald/rostislav/relationshipapp/database/AppDatabase.kt
+++ app/src/main/java/cz/osvald/rostislav/relationshipapp/database/AppDatabase.kt
@@ -1,5 +1,6 @@
 package cz.osvald.rostislav.relationshipapp.database
 
+import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
@@ -8,5 +9,18 @@ import cz.osvald.rostislav.relationshipapp.database.entitity.Movie
 
 @Database(entities = [Movie::class], version = 1)
 abstract class AppDatabase : RoomDatabase() {
+    companion object {
+        private var instance: AppDatabase? = null
+        fun getInstance(context: Context): AppDatabase = instance ?: synchronized(this) {
+            instance ?: Room
+                .databaseBuilder(
+                    context.applicationContext, AppDatabase::class.java, "relationship-app.db"
+                )
+                .build()
+                .also { instance = it }
+        }
+
+    }
+
     abstract fun movieDao(): MovieDao
 }
