15d13d4fcdd07decc8c2a5e93265b8d09c4cea4f
Rostislav Osvald <rostislav.osvald@gmail.com>
2021-11-10 17:21:56 +0100

    Fix MovieDao queries and make result LiveData

--- app/src/main/java/cz/osvald/rostislav/relationshipapp/database/dao/MovieDao.kt
+++ app/src/main/java/cz/osvald/rostislav/relationshipapp/database/dao/MovieDao.kt
@@ -1,18 +1,16 @@
 package cz.osvald.rostislav.relationshipapp.database.dao
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
+import androidx.room.Insert
 import androidx.room.Query
 import cz.osvald.rostislav.relationshipapp.database.entitity.Movie
 
 @Dao
 interface MovieDao: BaseDao<Movie> {
-    @Query("SELECT * FROM movie")
-    fun getAll(): List<Movie>
+    @Query("SELECT * FROM Movie WHERE viewedAt is NULL")
+    fun getMoviesToWatch(): LiveData<List<Movie>>
 
-    @Query("SELECT * FROM movie WHERE viewedAt = NULL")
-    fun getMoviesToWatch(): List<Movie>
-
-    @Query("SELECT * FROM movie WHERE viewedAt <> NULL")
-    fun getWatchedMovies(): List<Movie>
-
+    @Query("SELECT * FROM Movie WHERE viewedAt is not NULL")
+    fun getWatchedMovies(): LiveData<List<Movie>>
 }
