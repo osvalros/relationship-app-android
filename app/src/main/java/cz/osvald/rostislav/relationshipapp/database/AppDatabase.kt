4693d2a77b3e14d588722984e6ab4b26258b62ed
Rostislav Osvald <rostislav.osvald@gmail.com>
2021-11-10 17:20:47 +0100

    Add converter for database LocalDateTime objects

--- app/src/main/java/cz/osvald/rostislav/relationshipapp/database/AppDatabase.kt
+++ app/src/main/java/cz/osvald/rostislav/relationshipapp/database/AppDatabase.kt
@@ -1,13 +1,33 @@
 package cz.osvald.rostislav.relationshipapp.database
 
 import android.content.Context
-import androidx.room.Database
-import androidx.room.Room
-import androidx.room.RoomDatabase
+import androidx.room.*
 import cz.osvald.rostislav.relationshipapp.database.dao.MovieDao
 import cz.osvald.rostislav.relationshipapp.database.entitity.Movie
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+
+object DateTimeTypeConverters {
+    private val formatter = DateTimeFormatter.ISO_LOCAL_DATE_TIME
+
+    @TypeConverter
+    @JvmStatic
+    fun toOffsetDateTime(value: String?): LocalDateTime? {
+        return value?.let {
+            return formatter.parse(value, LocalDateTime::from)
+        }
+    }
+
+    @TypeConverter
+    @JvmStatic
+    fun fromOffsetDateTime(date: LocalDateTime?): String? {
+        return date?.format(formatter)
+    }
+}
 
 @Database(entities = [Movie::class], version = 1)
+@TypeConverters(DateTimeTypeConverters::class)
 abstract class AppDatabase : RoomDatabase() {
     companion object {
         private var instance: AppDatabase? = null
