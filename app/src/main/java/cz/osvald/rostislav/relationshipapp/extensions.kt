4aa554f136a4c345ed198c0816fbfd98711feaff
Rostislav Osvald <rostislav.osvald@gmail.com>
2021-12-30 18:05:17 +0100

    Add FRAGMENT_LOG_TAG alias for LOG_TAG so its easier to reference to it from other scope

--- app/src/main/java/cz/osvald/rostislav/relationshipapp/extensions.kt
+++ app/src/main/java/cz/osvald/rostislav/relationshipapp/extensions.kt
@@ -1,7 +1,12 @@
 package cz.osvald.rostislav.relationshipapp
 
+import androidx.fragment.app.Fragment
+
 val Any.LOG_TAG: String
     get() {
         val tag = javaClass.simpleName
         return if (tag.length <= 23) tag else tag.substring(0, 23)
     }
+
+val Fragment.FRAGMENT_LOG_TAG: String
+    get() = LOG_TAG
