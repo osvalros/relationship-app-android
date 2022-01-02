b4d13b368d71cb875aba4c66a8cdda6860bc7cb9
Rostislav Osvald <rostislav.osvald@gmail.com>
2022-01-02 18:33:38 +0100

    Add LocalDateTime type adapter for retrofit

--- app/src/main/java/cz/osvald/rostislav/relationshipapp/service/ApiClient.kt
+++ app/src/main/java/cz/osvald/rostislav/relationshipapp/service/ApiClient.kt
@@ -1,15 +1,43 @@
 package cz.osvald.rostislav.relationshipapp.service
 
-import com.google.gson.Gson
-import com.google.gson.GsonBuilder
+import com.google.gson.*
 import cz.osvald.rostislav.relationshipapp.BuildConfig
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.lang.reflect.Type
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+object LocalDateTimeGsonTypeAdapter : JsonSerializer<LocalDateTime?>,
+    JsonDeserializer<LocalDateTime?> {
+    private val formatter = DateTimeFormatter.ISO_LOCAL_DATE_TIME
+
+    override fun serialize(
+        src: LocalDateTime?,
+        typeOfSrc: Type?,
+        context: JsonSerializationContext?
+    ): JsonElement {
+        return JsonPrimitive(src?.format(formatter))
+    }
+
+    override fun deserialize(
+        json: JsonElement?,
+        typeOfT: Type?,
+        context: JsonDeserializationContext?
+    ): LocalDateTime? {
+        return json?.asString?.let {
+            return formatter.parse(it, LocalDateTime::from)
+        }
+    }
+}
 
 object ApiClient {
     private val gson: Gson by lazy {
-        GsonBuilder().setLenient().create()
+        GsonBuilder()
+            .setLenient()
+            .registerTypeAdapter(LocalDateTime::class.java, LocalDateTimeGsonTypeAdapter)
+            .create()
     }
 
     private val httpClient: OkHttpClient by lazy {
