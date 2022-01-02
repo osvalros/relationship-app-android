613cf5a50ee19fdcad16a125e3eb2e9a8166b478
Rostislav Osvald <rostislav.osvald@gmail.com>
2022-01-02 18:34:39 +0100

    Update data types for datetimes

--- app/src/main/java/cz/osvald/rostislav/relationshipapp/models/EditMovieBody.kt
+++ app/src/main/java/cz/osvald/rostislav/relationshipapp/models/EditMovieBody.kt
@@ -1,6 +1,8 @@
 package cz.osvald.rostislav.relationshipapp.models
 
+import java.time.LocalDateTime
+
 data class EditMovieBody(
     val name: String,
-    val viewedAt: String,
+    val viewedAt: LocalDateTime,
 )
--- app/src/main/java/cz/osvald/rostislav/relationshipapp/models/Movie.kt
+++ app/src/main/java/cz/osvald/rostislav/relationshipapp/models/Movie.kt
@@ -1,8 +1,10 @@
 package cz.osvald.rostislav.relationshipapp.models
 
+import java.time.LocalDateTime
+
 data class Movie(
     val id: Int,
     val name: String,
-    val createdAt: String?,
-    val viewedAt: String?
+    val createdAt: LocalDateTime,
+    val viewedAt: LocalDateTime?
 )
